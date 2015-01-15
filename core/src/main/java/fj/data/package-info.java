75587ba29c1b35433cbb68d9a2012c67ae691ad2
Dobes Vandermeer <dobesv@gmail.com>
2015-01-14 23:51:08 -0800

    Make as many things non-null by default as possible

--- core/src/main/java/fj/data/package-info.java
+++ core/src/main/java/fj/data/package-info.java
@@ -3,5 +3,13 @@
  *
  * @version %build.number%
  */
-@org.eclipse.jdt.annotation.NonNullByDefault
+@org.eclipse.jdt.annotation.NonNullByDefault({PARAMETER, RETURN_TYPE, FIELD, TYPE_BOUND, TYPE_ARGUMENT, TYPE_PARAMETER, ARRAY_CONTENTS})
 package fj.data;
+import static org.eclipse.jdt.annotation.DefaultLocation.FIELD;
+import static org.eclipse.jdt.annotation.DefaultLocation.PARAMETER;
+import static org.eclipse.jdt.annotation.DefaultLocation.RETURN_TYPE;
+import static org.eclipse.jdt.annotation.DefaultLocation.TYPE_ARGUMENT;
+import static org.eclipse.jdt.annotation.DefaultLocation.TYPE_BOUND;
+import static org.eclipse.jdt.annotation.DefaultLocation.ARRAY_CONTENTS;
+import static org.eclipse.jdt.annotation.DefaultLocation.TYPE_PARAMETER;
+
